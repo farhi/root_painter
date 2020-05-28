09a7443d3900611da9c6dc7b57369b9ae442225b
Abraham George Smith <abe@abesmith.co.uk>
2020-05-28 18:54:55 +0200

    multiclass dice loss seems to work :)

--- trainer/loss.py
+++ trainer/loss.py
@@ -19,54 +19,30 @@ import torch
 from torch.nn.functional import softmax
 from torch.nn.functional import cross_entropy
 
-cx_loss = torch.nn.CrossEntropyLoss()
 
-def dice_loss(predictions, labels, eps=1e-7):
+def dice_loss(class_preds, class_labels):
     """ based on loss function from V-Net paper """
+    assert class_labels.dtype == torch.float32
+    preds = class_preds.contiguous().view(-1)
+    labels = class_labels.view(-1)
+    intersection = torch.sum(torch.mul(preds, labels))
+    union = torch.sum(preds) + torch.sum(labels)
+    return 1 - ((2 * intersection) / (union))
 
+
+def multiclass_loss(predictions, defined, labels):
+    """ mix of dice and cross entropy """
     classes = torch.unique(labels).long()
-    dices = torch.zeros(len(classes)).cuda()
+    # remove any of the predictions for which we don't have ground truth
+    # Set outputs to 0 where annotation undefined so that
+    # The network can predict whatever it wants without any penalty.
+    for c in range(predictions.shape[1]):
+        predictions[:, c] *= defined
     softmaxed = softmax(predictions, 1)
-
-    assert len(dices) == len(classes), f"{len(dices)},{len(classes)}"
-    for c in range(len(classes)):
-        # for each of the labels defined in the annotation.
-        # get the specific class prediction
-        class_preds = softmaxed[:, c, :]  
-        class_label_map = (labels == c)
-        class_label_map = class_label_map.float()
-        class_preds = class_preds.contiguous().view(-1)
-        class_label_map = class_label_map.view(-1)
-        intersection = torch.sum(torch.mul(class_preds, class_label_map))
-        union = torch.sum(class_preds) + torch.sum(class_label_map)
-        dices[c] = 1 - ((2 * intersection) / (union + eps))
-    return torch.mean(dices)
-
-
-def combined_loss(predictions, defined, labels):
-    """ mix of dice and cross entropy 
-    
-    Shapes of input:
-        predictions : (bs, classes, out_tile_w, out_tile_w)
-        defined: (bs, out_tile_w, out_tile_w))
-        labels: (bs, out_tile_w, out_tile_w)
-
-    """
-    # regions which are not defined are always set to 0
-    # having both the predictions and labels set to 0 for these regions
-    # means only the defined regions will be taken into account for model updates.
-    for i in range(1, predictions.shape[1]):
-        predictions[:, i] *= defined
-
-    # this is not exactly 0 but it is close. See loss flooding for some ideas
-    # why always having a bit more loss might be handy.
-    # https://arxiv.org/pdf/2002.08709.pdf
-    # TODO. Don't do this. There has to be a better way. this is probably screwing things up 
-    predictions[:, 0] += ((torch.ones(defined.shape).cuda() - defined) * 80)
-    # undefined region should be 0
-    labels *= defined.long()
-
-    if torch.sum(labels) > 0:
-        return dice_loss(predictions, labels) + (0.3 * cx_loss(predictions, labels))
-    # When only background then use only cross entropy as dice is undefined.
-    return 0.3 * cx_loss(predictions, labels)
+    dice_loss_sum = 0
+    for c in classes:
+        class_preds = softmaxed[:, c]
+        class_labels = (labels == c).float()
+        dice_loss_sum += dice_loss(class_preds, class_labels)
+    dice_loss_sum /= len(classes)
+    return dice_loss_sum + (0.3 * cross_entropy(predictions, labels))
