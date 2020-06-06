27244e2a84a5347cb92d177c137fd0d3f19aa075
Abraham George Smith <abe@abesmith.co.uk>
2020-06-06 14:44:33 +0200

    testing fit random noise and pylint train tests

--- trainer/tests/test_train_3d.py
+++ trainer/tests/test_train_3d.py
@@ -16,9 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import time
 
-import numpy as np
 import torch
 import torch.nn.functional as F
 import pytest
@@ -35,9 +33,8 @@ def test_train_to_predict_zeros_from_random():
     num_classes = 3
     cnn = UNet3D(im_channels=1, out_channels=num_classes).cuda()
     test_input = torch.rand((1, 1, 56, 240, 240)).cuda()
-    # bs = 1,
-    target = torch.zeros((1, 18, 194, 194)).cuda().long()
-    start = time.time()
+    batch_size = 1
+    target = torch.zeros((batch_size, 18, 194, 194)).cuda().long()
     outputs = cnn(test_input)
     assert outputs.shape[1] == num_classes
     optimizer = torch.optim.SGD(cnn.parameters(), lr=0.01, momentum=0.99, nesterov=True)
@@ -59,15 +56,14 @@ def test_train_to_predict_ones_from_random():
     """
     num_classes = 31
     cnn = UNet3D(im_channels=1, out_channels=num_classes).cuda()
-    bs = 1
-    test_input = torch.rand((bs, 1, 56, 240, 240)).cuda()
-    target = torch.ones((bs, 18, 194, 194)).cuda().long()
-    start = time.time()
+    batch_size = 1
+    test_input = torch.rand((batch_size, 1, 56, 240, 240)).cuda()
+    target = torch.ones((batch_size, 18, 194, 194)).cuda().long()
     outputs = cnn(test_input)
     assert outputs.shape[1] == num_classes
-    assert outputs.shape[0] == bs
+    assert outputs.shape[0] == batch_size
     optimizer = torch.optim.SGD(cnn.parameters(), lr=0.01, momentum=0.99, nesterov=True)
-    for i in range(100):
+    for _ in range(100):
         optimizer.zero_grad()
         outputs = cnn(test_input)
         loss = F.cross_entropy(outputs, target)
@@ -80,4 +76,43 @@ def test_train_to_predict_ones_from_random():
         if len(unique) == 1 and diff_from_1s == 0:
             print('predicted all 1s')
             return
-    assert False, 'test took too long' 
+    assert False, 'test took too long'
+
+
+@pytest.mark.slow
+def test_train_identity_from_random():
+    """
+    Very simple test to check that we can train the CNN model to
+    map input to output
+
+    It cannot actually do this but we can test that it gets close.
+
+    """
+    cnn = UNet3D(im_channels=1, out_channels=1).cuda()
+    batch_size = 1
+    # small input for this test to make it go faster.
+    test_input = torch.rand((batch_size, 1, 56, 56, 56)).cuda()
+    target = test_input[:, :, 19:-19, 19:-19, 19:-19]
+    outputs = cnn(test_input)
+    assert outputs.shape[0] == batch_size
+    optimizer = torch.optim.SGD(cnn.parameters(), lr=0.01, momentum=0.99, nesterov=True)
+    criterion = torch.nn.MSELoss() # see [1]
+
+    for _ in range(100):
+        optimizer.zero_grad()
+        outputs = cnn(test_input)
+        # Also consider MarginRankingLoss for similar problems, see [0]
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+        diff_mean = torch.mean(torch.abs(outputs - target))
+        if diff_mean < 0.1:
+            return
+    assert False, 'test took too long'
+
+# pylint: disable=W0105 # string has no effect
+# pylint: disable=C0301 # line too long
+"""
+[0] https://discuss.pytorch.org/t/how-to-calculate-pair-wise-differences-between-two-tensors-in-a-vectorized-way/37451/3
+[1] https://medium.com/pytorch/implementing-an-autoencoder-in-pytorch-19baa22647d1
+"""
