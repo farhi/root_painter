05bb66574728151615ba60c0074aeae2e15c3d2b
Abraham George Smith <abe@abesmith.co.uk>
2020-06-07 16:14:44 +0200

    adding plotting code for results from training on one patch

--- trainer/tests.py
+++ trainer/tests.py
@@ -24,7 +24,8 @@ run_single = True
 if run_single:
     import sys
     sys.path.insert(1, 'tests')
-    from test_train_3d import test_train_struct_seg_heart_patch as single_test
+    # from test_train_3d import test_train_struct_seg_heart_patch as single_test
+    from test_train_3d import test_train_struct_seg_heart_and_lung_patch as single_test
     single_test()
 else:
     test_dir = pathlib.Path.cwd() / 'tests'
