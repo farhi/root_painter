09d59a08c84e1c13ebc9fc56a78d2b6aa1da2a30
Abraham George Smith <abe@abesmith.co.uk>
2020-06-08 21:25:22 +0200

    plot number of heart and esophagus voxels

--- trainer/image_class_balance.py
+++ trainer/image_class_balance.py
@@ -62,3 +62,64 @@ plt.ylabel('number of voxels')
 plt.xlabel('class')
 plt.xticks(range(len(names)), names)
 plt.savefig('image_class_balance_foreground.png')
+
+
+plt.figure(figsize=(12*0.8, 8*0.8))
+plt.ylabel('number of esophagus voxels')
+plt.xlabel('Image (ordered by esophagus size)')
+vals = []
+names = []
+for i in range(1, 51):
+    data_dir = os.path.join(Path.home(), 'datasets', 'Thoracic_OAR', str(i))
+    annot, _ = im_utils.load_image(os.path.join(data_dir, 'label.nii.gz'))
+    vals.append(np.sum(annot==4))
+    names.append(str(i))
+
+
+plt.grid()
+vals, names = zip(*sorted(zip(vals, names)))
+rects = plt.bar(list(range(len(vals))), vals, color=([plt.rcParams['axes.prop_cycle'].by_key()['color'][4]] * 50))
+#autolabel(rects)
+#plt.xticks(range(len(names)), names)
+plt.savefig('esophagus_sizes.png')
+
+### as %
+plt.figure(figsize=(12*0.8, 8*0.8))
+plt.ylabel('Esophagus % of image')
+plt.xlabel('Image (ordered by esophagus size)')
+vals = []
+names = []
+for i in range(1, 51):
+    data_dir = os.path.join(Path.home(), 'datasets', 'Thoracic_OAR', str(i))
+    annot, _ = im_utils.load_image(os.path.join(data_dir, 'label.nii.gz'))
+    vals.append(100 * ((np.sum(annot==4)) / annot.size))
+    names.append(str(i))
+
+
+plt.grid()
+vals, names = zip(*sorted(zip(vals, names)))
+rects = plt.bar(list(range(len(vals))), vals, color=([plt.rcParams['axes.prop_cycle'].by_key()['color'][4]] * 50))
+#autolabel(rects)
+#plt.xticks(range(len(names)), names)
+plt.savefig('esophagus_percent.png')
+
+
+### Heart % of image.
+plt.figure(figsize=(12*0.8, 8*0.8))
+plt.ylabel('Heart % of image')
+plt.xlabel('Image (ordered by heart size)')
+vals = []
+names = []
+for i in range(1, 51):
+    data_dir = os.path.join(Path.home(), 'datasets', 'Thoracic_OAR', str(i))
+    annot, _ = im_utils.load_image(os.path.join(data_dir, 'label.nii.gz'))
+    vals.append(100 * ((np.sum(annot==3)) / annot.size))
+    names.append(str(i))
+
+plt.grid()
+vals, names = zip(*sorted(zip(vals, names)))
+rects = plt.bar(list(range(len(vals))), vals, color=([plt.rcParams['axes.prop_cycle'].by_key()['color'][3]] * 50))
+#autolabel(rects)
+#plt.xticks(range(len(names)), names)
+plt.savefig('heart_percent.png')
+
