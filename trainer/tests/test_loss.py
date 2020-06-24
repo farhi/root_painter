74f0fe174bb87e22df9676f0f818e3706a277df6
Abraham George Smith <abe@abesmith.co.uk>
2020-06-24 16:33:32 +0200

    lint fix for tests/test_loss.py

--- trainer/tests/test_loss.py
+++ trainer/tests/test_loss.py
@@ -16,8 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 # pylint: disable=E0401 # import error
-from loss import combined_loss
 import torch
+from loss import combined_loss
 
 
 def test_low_loss_when_matching_labels():
